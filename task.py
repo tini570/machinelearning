dd6c8d9d2c64d61e02150c22fb2566cd7d1f3de6
kezia <keziaannmathew@cs.ajce.in>
2017-10-24 15:01:55 +0530

    first commit

---
+++ task.py
@@ -0,0 +1,28 @@
+list=[]
+n=0
+for i in range(0,6):
+  list[i]=int(input())
+for n in range(0,5):
+  if n==0:
+    print("appending...")
+    print("enter the value to be appended")
+    print(list.append(int(input()))
+    print(list)
+  elif n==1:
+    print("reversing...")
+    print(list.reverse())
+    print(list)
+  elif n==2:
+    print("sorting...")
+    print(list.sort())
+    print(list)
+  elif n==3:
+    print(popping)
+    print(list.pop())
+    print(list)
+  elif n==4:
+    print("counting...")
+    print("enter the value to be counted")
+    print(list.count(int(input())))
+    print(list)
+ 
